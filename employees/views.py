10ffc2d0cdcdf6b89200edefe55806f9f8a75779
BinCha1 <contactwithbinisha@gmail.com>
2025-09-14 11:52:36 +0545

    fetch emp detail, mk emp  detail page

--- employees/views.py
+++ employees/views.py
@@ -6,4 +6,5 @@ from .models import Employee
 # Create your views here.
 def employee_detail(request,pk):
     employee=get_object_or_404(Employee, pk=pk)
-    return HttpResponse(employee)
+    context={'employee':employee}   
+    return render(request,'employee_details.html',context)
--- firstproject/urls.py
+++ firstproject/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home),  # you need to import views from your app
+    path("", views.home, name="home"),  # you need to import views from your app
     path('employees/', include('employees.urls')),  # include the URLs from employees app forward req to employees/urls.py 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # this is for serving media files in development
